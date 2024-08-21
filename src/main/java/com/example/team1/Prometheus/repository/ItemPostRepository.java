bfe987d2a3d8353cdf8e43a69ae47dee033157d5
Sur0n <skyzephyr333@gmail.com>
2024-08-21 18:06:59 +0900

    Feat: RegisterItemService 클래스 추가

---
+++ src/main/java/com/example/team1/Prometheus/repository/ItemPostRepository.java
@@ -0,0 +1,5 @@
+package com.example.team1.Prometheus.repository;
+
+public interface ItemPostRepository {
+    //TODO 데이터베이스 연결
+}
---
+++ src/main/java/com/example/team1/Prometheus/service/RegisterItemService.java
@@ -0,0 +1,16 @@
+package com.example.team1.Prometheus.service;
+
+import com.example.team1.Prometheus.repository.ItemPostRepository;
+
+public class RegisterItemService {
+    //TODO 체크
+//    private final ItemPostRepository itemPostRepository;
+//
+//    public Contact createItem(Contact contact) {
+//        log.debug("새로운 연락처 생성: {}", contact.getName());
+//
+//        Contact savedContact = contactRepository.save(contact);
+//
+//        return savedContact;
+//    }
+}
