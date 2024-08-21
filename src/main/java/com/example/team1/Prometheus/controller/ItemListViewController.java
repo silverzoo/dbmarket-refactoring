999a13705c4dbf4567506fde27e435ade91530c0
[CLOUD4] 안송희 <songhee9327@gmail.com>
2024-08-21 19:23:06 +0900

    ✨ Feat: 아이템 컨트롤러 추가

---
+++ src/main/java/com/example/team1/Prometheus/controller/ItemListViewController.java
@@ -0,0 +1,28 @@
+package com.example.team1.Prometheus.controller;
+
+import com.example.team1.Prometheus.entity.ItemListViewResponse;
+import com.example.team1.Prometheus.service.ItemListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+//아이템 목록 조회/표시
+@Controller
+@RequiredArgsConstructor
+public class ItemListViewController {
+    private final ItemListService itemListService;
+
+    @GetMapping("/itmes")
+    public String getAllItems(Model model){
+        List<ItemListViewResponse> items = itemListService.getAllItems();
+        model.addAttribute("items",items);
+        return "items";
+    }
+
+
+
+}
