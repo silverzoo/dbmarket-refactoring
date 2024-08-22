1e0f8766db421fee1c7cf96acf4bd8decb9cf1e2
Sur0n <skyzephyr333@gmail.com>
2024-08-23 04:25:09 +0900

    ✨ Feat: 상품 등록 post jpa 연결 #13

--- src/main/java/com/example/team1/Prometheus/repository/ItemPostRepository.java
+++ src/main/java/com/example/team1/Prometheus/repository/ItemPostRepository.java
@@ -1,5 +1,10 @@
 package com.example.team1.Prometheus.repository;
 
-public interface ItemPostRepository {
-    //TODO 데이터베이스 연결
+import com.example.team1.Prometheus.entity.ItemPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//TODO 데이터베이스 연결
+public interface ItemPostRepository extends JpaRepository<ItemPost, Long>{
 }
+
+
