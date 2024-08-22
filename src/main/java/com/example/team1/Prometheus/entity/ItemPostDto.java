79bb3d5e117daadf51353cbf2d16ea4b9e9c6428
Sur0n <skyzephyr333@gmail.com>
2024-08-23 04:28:36 +0900

    ✨ Feat: 상품 등록 Entity,Dto 추가 #13

--- src/main/java/com/example/team1/Prometheus/entity/ItemPostDto.java
+++ src/main/java/com/example/team1/Prometheus/entity/ItemPostDto.java
@@ -1,23 +1,25 @@
 package com.example.team1.Prometheus.entity;
 
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.awt.*;
-@AllArgsConstructor
-@NoArgsConstructor
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
 @Setter
+@Getter
 public class ItemPostDto{
     private String name;
     private int price;
-    private String Category;
-    private Image image;
+    private String category;
+    private String imagePath;
     private String description;
 
-    public ItemPost toEntity() {
-        ItemPost itemPost = new ItemPost(name,price,Category,image,description);
-        return itemPost;
+    public ItemPost toEntity(){
+        return new ItemPost(null,name,price,category,imagePath,description,LocalDateTime.now());
     }
 }
