bfb0ee5316ea1dff286ca3cab22d1d3c13f6baf6
Sur0n <skyzephyr333@gmail.com>
2024-08-23 04:27:20 +0900

    ✨ Feat: 상품 DB, 이미지 저장 Service 개발 #13 RegisterItemService

--- src/main/java/com/example/team1/Prometheus/service/RegisterItemService.java
+++ src/main/java/com/example/team1/Prometheus/service/RegisterItemService.java
@@ -1,50 +1,82 @@
 package com.example.team1.Prometheus.service;
 
+import com.example.team1.Prometheus.entity.ItemPostDto;
 import com.example.team1.Prometheus.repository.ItemPostRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.Part;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.UUID;
 
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Slf4j
 public class RegisterItemService {
     private ItemPostRepository itemPostRepository;
+    private ItemPostDto itemPostDto;
+
     public void uploadItemToDb(HttpServletRequest request) throws ServletException, IOException {
         // 작업을 service로 분리
 
+//        public Member createMember(Member member) {
+//            return memberRepository.save(member);
+//        }
         //servelet이 제공하는 part로 multipart를 확인
         Collection<Part> parts = request.getParts();
         log.info("parts={}", parts);
         for (Part part : parts) {
             log.info("==== PART ====");
+            //데이터 읽기
             log.info("name={}", part.getName());
-            Collection<String> headerNames = part.getHeaderNames();
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body={}", body);
             //TODO 2 DB저장하기
-            for (String headerName : headerNames) {
-                log.info("header {}: {}", headerName,
-                        part.getHeader(headerName));
-            }
-            //content-disposition; filename
-            log.info("submittedFileName={}", part.getSubmittedFileName());
-            log.info("size={}", part.getSize()); //part body size
+            //TODO 3 리팩토링
+            switch (part.getName()) {
+                case "name":
+                    itemPostDto.setName(body);
+                    break;
+                case "price":
+                    itemPostDto.setPrice(Integer.parseInt(body));
+                    break;
+                case "category":
+                    itemPostDto.setCategory(body);
+                    break;
+                case "imgFile":
 
-            //파일에 저장하기
-            if (StringUtils.hasText(part.getSubmittedFileName())) {
-                // 저장경로 설정해서 활용
+                    if (StringUtils.hasText(part.getSubmittedFileName())) {
+                        //편의 메서드
+                        log.info("size={}", part.getSize()); //part body size
+                        log.info("submittedFileName={}", part.getSubmittedFileName());
+                        //uuid만 저장하면 uuid 파일이 완성됨, 확장자 필요.
+                        String extention = part.getSubmittedFileName()
+                                .substring(part.getSubmittedFileName().lastIndexOf("."));
+                        // 저장경로 설정해서 활용
 //                String fullPath = fileDir + part.getSubmittedFileName();
-                String fullPath = part.getSubmittedFileName();
-                log.info("파일 저장 fullPath={}", fullPath);
-                part.write(fullPath);
+                        String fullPath = UUID.randomUUID().toString() + extention;
+//                        log.info("파일 저장 fullPath={}", fullPath);
+                        //파일 저장하기
+                        part.write(fullPath);
+                        //Dto 경로 저장
+                        itemPostDto.setImagePath(fullPath);
+                    }
+                    break;
+                case "description":
+                    itemPostDto.setDescription(body);
+                    break;
             }
         }
+        itemPostRepository.save(itemPostDto.toEntity());
     }
 }
