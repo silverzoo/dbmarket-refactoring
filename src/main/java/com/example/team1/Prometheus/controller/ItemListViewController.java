2d0e8bf79a8dce2c94f30b7bcb79fc5b22908732
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-08-23 15:52:18 +0900

    feature : 로그인 세션 추가

--- src/main/java/com/example/team1/Prometheus/controller/ItemListViewController.java
+++ src/main/java/com/example/team1/Prometheus/controller/ItemListViewController.java
@@ -1,10 +1,14 @@
 package com.example.team1.Prometheus.controller;
 
 import com.example.team1.Prometheus.entity.ItemListViewResponse;
+import com.example.team1.Prometheus.entity.User;
 import com.example.team1.Prometheus.service.ItemListService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +21,9 @@ public class ItemListViewController {
     private final ItemListService itemListService;
 
     @GetMapping("/items")
-    public String getAllItems(Model model){
+    public String getAllItems(Model model, HttpServletRequest httpServletRequest) {
+
+
         List<ItemListViewResponse> items = itemListService.getAllItems();
         model.addAttribute("items",items);
         return "items";
--- src/main/java/com/example/team1/Prometheus/entity/User.java
+++ src/main/java/com/example/team1/Prometheus/entity/User.java
@@ -10,7 +10,7 @@ import lombok.*;
 @ToString
 
 @Entity
-//@Table(name="users")
+@Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
