d1bdd7c567249cb8ee314e210c014b32c5603ea6
[CLOUD4] 안송희 <songhee9327@gmail.com>
2024-08-25 03:24:29 +0900

    🐛 Fix: 댓글 수정 코드

--- src/main/java/com/example/team1/Prometheus/service/CommentService.java
+++ src/main/java/com/example/team1/Prometheus/service/CommentService.java
@@ -6,6 +6,7 @@ import com.example.team1.Prometheus.entity.CommentResponse;
 import com.example.team1.Prometheus.entity.User;
 import com.example.team1.Prometheus.repository.CommentRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +17,9 @@ import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
-
-
-    public CommentService(CommentRepository commentRepository){
-        this.commentRepository = commentRepository;
-    }
 
     // 해당 아이디의 유저 모든 comment를 CommentResponse 리스트로 리턴
     public List<CommentResponse> getAllCommentById(Long userId) {
@@ -34,20 +31,15 @@ public class CommentService {
                 .collect(Collectors.toList());
     }
 
-    //해당 커멘트 아이디로 조히
+    //해당 커멘트 아이디로 조회
     public CommentResponse getCommentById(Long commentId) {
         return new CommentResponse(commentRepository.findById(commentId).orElse(null));
     }
 
     @Transactional
-    public CommentResponse updateComment(Long commentId, CommentRequest commentRequest,String userName){
+    public CommentResponse updateComment(Long commentId, CommentRequest commentRequest){
         //조회
         Comment comment = commentRepository.findById(commentId).orElse(null);
-
-        //댓글 작성자의 이름과 세션의 유저이름이 같지않으면 작성 권한이 없음
-        if (!comment.getReviewerName().equals(userName)) {
-            throw new SecurityException("댓글 수정 권한이 없습니다.");
-        }
 
         //요청 객체 내용 엔티티에 매핑
         comment.setContent(commentRequest.getContent());
@@ -58,16 +50,9 @@ public class CommentService {
 
     }
 
-    //세션 유저 이름이 커멘트의 리뷰어 이름이랑 같으면 삭제
-    @Transactional
-    public void deleteComment(Long commentId, String userName){
-        Comment comment = commentRepository.findById(commentId).orElse(null);
 
-        if (!comment.getReviewerName().equals(userName)) {
-            throw new SecurityException("댓글 삭제 권한이 없습니다.");
-        }
-
-        commentRepository.delete(comment);
+    public void deleteComment(Long commentId){
+        commentRepository.deleteById(commentId);
     }
 
 
