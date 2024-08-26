7eb1b76db7d4f086df4b7b6af269381c6bf0fec7
[cloud4] 이은주 <silverzoo030@gmail.com>
2024-08-26 15:33:06 +0900

    Refactor: form 으로 delete 메서드 사용하기

--- src/main/java/com/example/team1/Prometheus/controller/ItemDetailViewController.java
+++ src/main/java/com/example/team1/Prometheus/controller/ItemDetailViewController.java
@@ -1,5 +1,6 @@
 package com.example.team1.Prometheus.controller;
 
+import com.example.team1.Prometheus.entity.ItemDeleteResponse;
 import com.example.team1.Prometheus.entity.ItemModifyRequest;
 import com.example.team1.Prometheus.entity.ItemModifyResponse;
 import com.example.team1.Prometheus.entity.ItemResponse;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -29,7 +31,8 @@ public class ItemDetailViewController {
 
         ItemResponse item = itemDetailService.findById(id);
         model.addAttribute("item", item);
-        log.info("\n\n 유저아이디 정보 확인: " + item.getUserId() + "\n\n");
+
+        log.info("\n\n아이템 확인: {}\n\n", item);
 
         return "itemdetail/item";
     }
@@ -41,10 +44,12 @@ public class ItemDetailViewController {
         // 세션
         userFilter.findUserByFilter(model);
         String refer = userFilter.getHeader(req);
+
         log.info("\n\n상세 조회에서 세션 헤더 정보: {}\n\n", refer);
 
         ItemResponse item = itemDetailService.findById(id);
         model.addAttribute("item", item);
+
         return "itemdetail/edit";
     }
 
@@ -52,13 +57,32 @@ public class ItemDetailViewController {
     @PostMapping("/{id}")
     public String updateItem(@PathVariable("id") Long id, @ModelAttribute ItemModifyRequest request) {
 
-        // FIXME: 뷰에 값은 잘 전송되는데 로그로는 왜 null만 뜨는지?
-        log.info("\n\n상품 수정 되었나요?: {}\n\n", itemDetailService.updateItem(id, request));
+        ItemModifyResponse res = itemDetailService.updateItem(id, request);
+
+        log.info("\n\n상품 수정 확인: {}\n\n", res);
+
         return "redirect:/items/" + id;
     }
 
+    @DeleteMapping("/{id}")
+    public String deleteItem(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
 
+        try {
+            ItemDeleteResponse res = itemDetailService.deleteItem(id);
+            log.info("\n\n상품 삭제 확인: {}\n\n", res);
 
+            redirectAttributes.addFlashAttribute("message", "삭제되었습니다.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "삭제 실패: " + e.getMessage());
+        }
+
+        return "redirect:/items";
+    }
 
     // TODO: 판매자 정보페이지에서 삭제 시, 판매자 정보 뷰로 돌아가게 하기
+//    @RequestMapping("/redirectAfterDelete")
+//    public String redirectAfterDelete() {
+//        return "items";
+//    }
+
 }
