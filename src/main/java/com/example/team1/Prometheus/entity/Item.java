eae0cd716658d2ba7c23c4c57120f894c654b444
[cloud4] 이은주 <silverzoo030@gmail.com>
2024-08-26 15:32:01 +0900

    Fix: Item Entity <-> DTO 바인딩 오류 해결 close #42

--- src/main/java/com/example/team1/Prometheus/entity/Item.java
+++ src/main/java/com/example/team1/Prometheus/entity/Item.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 @Table(name="item")
 @Entity
 @Getter
-@Setter
 @Builder
 @ToString
 @NoArgsConstructor
--- src/main/java/com/example/team1/Prometheus/mapper/ItemMapper.java
+++ src/main/java/com/example/team1/Prometheus/mapper/ItemMapper.java
@@ -2,15 +2,12 @@ package com.example.team1.Prometheus.mapper;
 
 import com.example.team1.Prometheus.entity.*;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
 
-    ItemMapper itemMapper = Mappers.getMapper(ItemMapper.class);
-
     Item toEntity(ItemModifyRequest request);
-    ItemModifyResponse toResponse(Item item);
+    ItemModifyResponse toModifyResponse(Item item);
 
     ItemDeleteResponse toDeleteResponse(Item item);
 
