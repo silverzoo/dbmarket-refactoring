1f6c317d4f831962e7ef2a6394ec565d6ca7e25c
Sur0n <skyzephyr333@gmail.com>
2024-08-29 06:12:07 +0900

    setting : 서버 이미지 저장경로, 요청 url handler 지정

--- src/main/java/com/example/team1/Prometheus/WebConfig.java
+++ src/main/java/com/example/team1/Prometheus/WebConfig.java
@@ -1,12 +1,15 @@
 package com.example.team1.Prometheus;
 
+import com.example.team1.Prometheus.property.FileUploadProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final FileUploadProperties fileUploadProperties;
     //뷰 컨트롤러는 잘 모르겠음
 //    @Override
 //    public void addViewControllers(ViewControllerRegistry registry) {
@@ -15,12 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
 
 
     //이 부분이 없으면 사진 올렸을때 새로고침 안하면 사진 못불러옴
-    private final String rootPath = System.getProperty("user.dir");
-    private final String fileDir = rootPath + "/src/main/resources/static/upload/";
+    private String dir;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file:/"+fileDir);
+        dir = fileUploadProperties.getRootPath()+ fileUploadProperties.getDir();
+
+        //클라이언트가 파일에 접근하기 위한 url
+        registry.addResourceHandler(fileUploadProperties.getImagePath()+"**")
+                .addResourceLocations("file://"+dir);
 //        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
