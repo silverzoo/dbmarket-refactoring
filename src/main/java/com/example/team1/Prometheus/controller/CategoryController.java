af480dcf1716c784804bc39c6e5a7de6633986dc
[cloud4] 이은주 <silverzoo030@gmail.com>
2024-08-28 20:10:06 +0900

    Feat: 카테고리 수정 폼 페이지 구현 중

--- src/main/java/com/example/team1/Prometheus/controller/CategoryController.java
+++ src/main/java/com/example/team1/Prometheus/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.example.team1.Prometheus.controller;
 
+import com.example.team1.Prometheus.entity.CategoryRequest;
 import com.example.team1.Prometheus.entity.CategoryResponse;
 import com.example.team1.Prometheus.entity.ItemResponse;
 import com.example.team1.Prometheus.service.CategoryService;
@@ -7,9 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,7 +28,13 @@ public class CategoryController {
     }
 
     @GetMapping("/edit")
-    public String updateCategory() {
+    public String createCategory() {
+
+        return "category/edit";
+    }
+
+    @GetMapping("/edit")
+    public String createCategory(@ModelAttribute("categoryRequest") CategoryRequest categoryRequest) {
 
         return "category/edit";
     }
