3d23f97b8a0b77a0813ba6d5fa2d14aef234140c
Sur0n <skyzephyr333@gmail.com>
2024-08-29 06:09:42 +0900

    refactor : 서버별 fileUpload 설정 app 추가

--- src/main/java/com/example/team1/Prometheus/PrometheusApplication.java
+++ src/main/java/com/example/team1/Prometheus/PrometheusApplication.java
@@ -1,11 +1,17 @@
 package com.example.team1.Prometheus;
 
+import com.example.team1.Prometheus.property.FileUploadProperties;
+import com.example.team1.Prometheus.service.RegisterItemService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileUploadProperties.class
+})
 public class PrometheusApplication {
 
 	public static void main(String[] args) {
