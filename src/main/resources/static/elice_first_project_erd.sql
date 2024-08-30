d014bbaaf06621aa26f4bf853a34ec11e5341d4b
Sur0n <skyzephyr333@gmail.com>
2024-08-30 12:38:20 +0900

    docs : erd 최신화, 빌드 오류 해결

--- src/main/resources/static/elice_first_project_erd.sql
+++ src/main/resources/static/elice_first_project_erd.sql
@@ -3,17 +3,26 @@ CREATE TABLE `item` (
   `user_id` INTEGER NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `price` INTEGER NOT NULL,
-  `category` ENUM ('PET_ITEM', 'CLOTHES', 'GADGET', 'BOOKS', 'BEAUTY') NOT NULL,
+  `categoryId` Long NOT NULL,
+  `category` VARCHAR(255) NOT NULL,
   `image_path` VARCHAR(255) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
   `created_at` timestamp DEFAULT (now()),
-  `updated_at` timestamp
+  `updated_at` timestamp DEFAULT (now())
+);
+
+CREATE TABLE `Category` (
+  `category_id` Integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `users` (
   `user_id` Integer PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `rate` double
 );
 
 CREATE TABLE `comment` (
@@ -21,35 +30,12 @@ CREATE TABLE `comment` (
   `user_id` Integer,
   `reviewername` varchar(255),
   `content` varchar(255),
-  `created_at` timestamp
+  `created_at` timestamp,
+  `rating` Long
 );
-
-CREATE TABLE `chats` (
-  `chat_id` varchar(255) PRIMARY KEY,
-  `seller_id` integer NOT NULL,
-  `buyer_id` integer NOT NULL,
-  `seller_chat_history` chats NOT NULL,
-  `buyer_chat_history` chats NOT NULL
-);
-
-CREATE TABLE `seller_chat_history` (
-  `seller_id` integer PRIMARY KEY,
-  `chat` varchar(255)
-);
-
-CREATE TABLE `buyer_chat_history` (
-  `buyer_id` integer PRIMARY KEY,
-  `chat` varchar(255)
-);
-
-ALTER TABLE `chats` ADD FOREIGN KEY (`seller_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `chats` ADD FOREIGN KEY (`buyer_id`) REFERENCES `users` (`user_id`);
-
-ALTER TABLE `chats` ADD FOREIGN KEY (`seller_id`) REFERENCES `seller_chat_history` (`seller_id`);
-
-ALTER TABLE `buyer_chat_history` ADD FOREIGN KEY (`buyer_id`) REFERENCES `chats` (`buyer_id`);
 
 ALTER TABLE `item` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 ALTER TABLE `comment` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `Category` ADD FOREIGN KEY (`category_id`) REFERENCES `item` (`categoryId`);
