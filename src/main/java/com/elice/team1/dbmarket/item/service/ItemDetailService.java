2a0a5645c9f96d24625570591e18b56e6deaa946
[cloud4] 이은주 <silverzoo030@gmail.com>
2024-09-29 18:12:34 +0900

    Refactor: ItemRepository로 통합

--- src/main/java/com/elice/team1/dbmarket/item/service/ItemDetailService.java
+++ src/main/java/com/elice/team1/dbmarket/item/service/ItemDetailService.java
@@ -12,7 +12,7 @@ import com.elice.team1.dbmarket.common.exception.NotFoundItemById;
 import com.elice.team1.dbmarket.common.exception.UnauthorizedDeleteByUser;
 import com.elice.team1.dbmarket.common.exception.UnauthorizedModifyByUser;
 import com.elice.team1.dbmarket.item.mapper.ItemMapper;
-import com.elice.team1.dbmarket.item.repository.ItemDetailRepository;
+import com.elice.team1.dbmarket.item.repository.ItemRepository;
 import com.elice.team1.dbmarket.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
@@ -24,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ItemDetailService {
-    private final ItemDetailRepository itemDetailRepository;
+    private final ItemRepository itemRepository;
     private final CategoryRepository categoryRepository;
     private final UserService userService;
     private final ItemMapper itemMapper;
@@ -32,7 +32,7 @@ public class ItemDetailService {
     @Transactional
     public ItemResponse viewItem(long id) {
 
-        Item item = itemDetailRepository.findById(id)
+        Item item = itemRepository.findById(id)
                 .orElseThrow(() ->  new NotFoundItemById(id));
 
         return itemMapper.toItemResponse(item);
@@ -41,7 +41,7 @@ public class ItemDetailService {
     @Transactional
     public ItemResponse findById(long id, HttpServletRequest httpServletRequest) {
 
-        Item item = itemDetailRepository.findById(id)
+        Item item = itemRepository.findById(id)
                 .orElseThrow(() -> new NotFoundItemById(id));
 
         Long userId = userService.getSessionUser(httpServletRequest).getId();
@@ -59,7 +59,7 @@ public class ItemDetailService {
     public ItemModifyResponse updateItem(long id, ItemModifyRequest request) {
 
         // 1. 엔티티를 데이터베이스에서 조회
-        Item existingItem = itemDetailRepository.findById(id)
+        Item existingItem = itemRepository.findById(id)
                 .orElseThrow(() -> new NotFoundItemById(id));
 
         log.info("\n\n수정 아이템 정보: {}\n\n", request.toString());
@@ -68,6 +68,9 @@ public class ItemDetailService {
         // 2. 카테고리 정보 가져오기
         Category category = categoryRepository.findById(request.getCategoryId())
                 .orElseThrow(() -> new NotFoundCategoryById(request.getCategoryId()));
+
+        // TODO: finalItem으로 새로 생성해서 빌더패턴 작성하지 않고 existingItem을 빌더패턴적용하고
+        // save 없이도 업데이트 가능한지 보기
 
         // 3. 빌더 패턴을 사용하여 수정된 엔티티 생성
         Item finalItem = Item.builder()
@@ -81,7 +84,7 @@ public class ItemDetailService {
                 .build();
 
         // 4. 업데이트된 엔티티를 저장
-        Item savedItem = itemDetailRepository.save(finalItem);
+        Item savedItem = itemRepository.save(finalItem);
 
         // 5. 저장된 엔티티를 DTO로 변환하여 반환
         return itemMapper.toModifyResponse(savedItem);
@@ -90,7 +93,7 @@ public class ItemDetailService {
     @Transactional
     public ItemDeleteResponse deleteItem(long id, HttpServletRequest httpServletRequest) {
 
-        Item item = itemDetailRepository.findById(id)
+        Item item = itemRepository.findById(id)
                 .orElseThrow(() -> new NotFoundItemById(id));
 
         Long userId = userService.getSessionUser(httpServletRequest).getId();
@@ -99,7 +102,7 @@ public class ItemDetailService {
             throw new UnauthorizedDeleteByUser(username);
         }
 
-        itemDetailRepository.deleteById(id);
+        itemRepository.deleteById(id);
 
         return itemMapper.toDeleteResponse(item);
     }
